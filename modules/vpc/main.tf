47279e08aef05d8b06628cea8bab71840e4cd56d
MasaNom22 <masakatsunomura26@gmail.com>
2026-02-14 16:08:56 +0900

    add igw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,3 +10,15 @@ resource "aws_vpc" "vpc" {
     }
   )
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-igw"
+    }
+  )
+}
